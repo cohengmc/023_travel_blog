4d8cf4b3a234d9cc93b5b13f1c24d968540ca9fc
Geoffrey <you@example.comcohengmc@gmail.com>
2024-06-03 11:09:06 -0400

    bloggin

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div>
+      <a href="./days/12MAY24.html">12MAY24</a>
+    </div>
+  );
+}
---
+++ src/days/12MAY24.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('12MAY24')).render(
+  <React.StrictMode>
+    <h1>HEY</h1>
+  </React.StrictMode>,
+)
