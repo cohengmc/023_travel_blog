b75c61fec35dcca90b9a53cb2d5470d294643aa0
Geoffrey <you@example.comcohengmc@gmail.com>
2024-06-05 17:30:30 -0400

    fav

--- src/days/13MAY24.jsx
+++ src/days/13MAY24.jsx
@@ -1,8 +1,11 @@
+import Navbar from ".././components/Navbar";
+
+
 export function MAY1324() {
     return (
 
         <div>
-
+            <Navbar />
             <h1>13MAY24</h1>
 
         </div>
--- src/days/14MAY24.jsx
+++ src/days/14MAY24.jsx
@@ -1,8 +1,10 @@
+import Navbar from ".././components/Navbar";
+
 export function MAY1424() {
     return (
 
         <div>
-
+            <Navbar />
             <h1>14MAY24</h1>
 
         </div>
