01ec8b211113e6ecd4bbccb72fc31b5bc04a9b98
Geoffrey <you@example.comcohengmc@gmail.com>
2024-06-09 08:57:42 -0400

    adding footer and making text visible

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Card from "./components/Card";
+import Footer from "./components/Footer";
 
 import "./Home.css";
 
@@ -46,6 +47,7 @@ export function Home() {
           />
         </Link>
       </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div id="footer">
+      <footer>
+        <h3>Thanks for checking out my page :)</h3>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
